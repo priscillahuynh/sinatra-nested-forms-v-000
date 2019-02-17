6f1d8ddb4b2cb437e740e210aa2d450959cc80b6
Priscilla H <priscillamhuynh@gmail.com>
2019-02-17 06:27:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+  get '/' do 
+   erb :root
+  end 
 
-    # code other routes/actions here
+  get '/new' do
+    erb :'pirates/new'
+  end 
+
+  post '/pirates' do 
+    @pirate = Pirate.new(params[:pirate])
+    @ship = Ship.new(params[:ship])
+    erb :'pirates/show'
+  end
 
   end
 end
