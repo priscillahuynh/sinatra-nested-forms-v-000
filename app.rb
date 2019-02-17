47d2f30891183c1604f7fc4f67a0fcd908ada8e7
Priscilla H <priscillamhuynh@gmail.com>
2019-02-17 06:11:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
-    # code other routes/actions here
-
+    get '/' do 
+      erb :root
+    end 
+    
+    get '/new' do
+      erb :new
+    end 
+    
+    post '/pirates' do 
+      @pirate = Pirate.new(params[:pirate])
+      @ship = Ship.new(params[:ship])
+      erb :show
+    end
+   
   end
 end
